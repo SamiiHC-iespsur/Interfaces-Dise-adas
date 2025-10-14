78d131bd1cb81c7de4df97793edb9c96a771e4e0
SamiiHC-iespsur <samuel.de.cozar.al@iespoligonosur.org>
2025-10-14 20:05:35 +0200

    Añadido proyecto de Interfaces

---
+++ ProyectoTarea1/src/main/java/com/mycompany/proyectotarea1/ProyectoTarea1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectotarea1;
+
+/**
+ *
+ * @author alumnadotarde
+ */
+public class ProyectoTarea1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
