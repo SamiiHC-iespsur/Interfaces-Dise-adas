a2b87299a2ddccafb601f1742c1d38af0313e83e
SamiiHC-iespsur <samuel.de.cozar.al@iespoligonosur.org>
2025-11-06 17:46:00 +0100

    Proyecto de componentes creado

---
+++ PswdComponente/src/main/java/com/mycompany/pswdcomponente/MedidorPswd.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pswdcomponente;
+
+/**
+ *
+ * @author alumnadotarde
+ */
+public class MedidorPswd {
+
+    // Valores posibles: 0 (inválida), 1, 2 (débil), 3, 4 (media), 5, 6 (fuerte), 7 (excelente)
+    public int medir(String pswd) {
+        int eval = 0;
+
+        // Si la contraseña tiene menos de 5 caracteres, se considera inválida
+        if (pswd.length() >= 5) {
+
+            // Comprobar si la contraseña contiene letras minúsculas
+            if (pswd.matches(".*[a-z].*")) {
+                eval++;
+            }
+
+            // Comprobar si la contraseña contiene letras mayúsculas
+            if (pswd.matches(".*[A-Z].*")) {
+                eval++;
+            }
+
+            // Comprobar si la contraseña contiene dígitos
+            if (pswd.matches(".*\\d.*")) {
+                eval++;
+            }
+
+            // Comprobar si la contraseña contiene caracteres especiales
+            if (pswd.matches(".*[!@#$%^&*(),.?\":{}|<>].*")) {
+                eval++;
+            }
+
+            // Comprobar si la contraseña tiene 8 o más caracteres
+            if (pswd.length() >= 8) {
+                eval++;
+
+                // Comprobar si la contraseña tiene 12 o más caracteres
+                if (pswd.length() >= 12) {
+                    eval++;
+
+                    // Comprobar si la contraseña tiene 16 o más caracteres
+                    if (pswd.length() >= 16) {
+                        eval++;
+                    }
+                }
+            }
+        }
+        return eval;
+    }
+}
